bf4e5fd2b888bf43084104856f0f0533720ee4b0
Scott Bickell <bickellworks@gmail.com>
2019-03-09 11:07:51 -0700

    Add fixed components

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,40 +1,6 @@
 import React, {Component} from 'react';
-// import TableHead from './components/TableHead';
-// import TableBody from './components/TableBody';
-
-const TableHead = () => {
-    return (
-        <thead>
-            <tr>
-                <th scope="col">Name</th>
-                <th scope="col">Job</th>
-            </tr>
-        </thead>
-    )
-}
-
-const TableBody = () => {
-    return (
-                <tbody>
-                     <tr>
-                         <td>Charlie</td>
-                         <td>Janitor</td>
-                     </tr>
-                     <tr>
-                         <td>Mac</td>
-                         <td>Bouncer</td>
-                     </tr>
-                     <tr>
-                         <td>Dee</td>
-                         <td>Aspiring actress</td>
-                     </tr>
-                     <tr>
-                         <td>Dennis</td>
-                         <td>Bartender</td>
-                     </tr>
-             </tbody>
-    )
-}
+import TableHead from './TableHead';
+import TableBody from './TableBody';
 
 class Table extends Component {
     render() {
