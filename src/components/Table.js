b0e6cf2b06c1a425b631d2766cd1ac613f420fac
Scott Bickell <bickellworks@gmail.com>
2019-03-08 21:16:48 -0700

    Add table fix

--- src/components/Table.js
+++ src/components/Table.js
@@ -1,6 +1,40 @@
 import React, {Component} from 'react';
-import TableHead from './components/TableHead';
-import TableBody from './components/TableBody';
+// import TableHead from './components/TableHead';
+// import TableBody from './components/TableBody';
+
+const TableHead = () => {
+    return (
+        <thead>
+            <tr>
+                <th scope="col">Name</th>
+                <th scope="col">Job</th>
+            </tr>
+        </thead>
+    )
+}
+
+const TableBody = () => {
+    return (
+                <tbody>
+                     <tr>
+                         <td>Charlie</td>
+                         <td>Janitor</td>
+                     </tr>
+                     <tr>
+                         <td>Mac</td>
+                         <td>Bouncer</td>
+                     </tr>
+                     <tr>
+                         <td>Dee</td>
+                         <td>Aspiring actress</td>
+                     </tr>
+                     <tr>
+                         <td>Dennis</td>
+                         <td>Bartender</td>
+                     </tr>
+             </tbody>
+    )
+}
 
 class Table extends Component {
     render() {
